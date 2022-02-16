0151834c32ec275768a3c10922bb045a2f4a48fe
JLeamanRye <98168311+JLeamanRye@users.noreply.github.com>
2022-02-16 18:59:54 -0500

    Update save_model.py

--- save_model.py
+++ save_model.py
@@ -21,7 +21,7 @@ flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 def save_tf():
   STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
 
-  input_layer = tf.keras.layers.Input([FLAGS.input_size, FLAGS.input_size, 3])
+  input_layer = tf.keras.layers.Input(5, 5, [FLAGS.input_size, FLAGS.input_size, 3])
   feature_maps = YOLO(input_layer, NUM_CLASS, FLAGS.model, FLAGS.tiny)
   tdOut = td(feature_maps)(input_layer) # LSTM Time Distro
   lstmOut = tf.keras.layers.LSTM(50, activation='tanh')(tdOut)
