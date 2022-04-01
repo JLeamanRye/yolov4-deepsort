5a9cdd5840d09fe2d66a8fb9800f7fa979a82f64
JLeamanRye <98168311+JLeamanRye@users.noreply.github.com>
2022-04-01 14:17:06 -0400

    Update yolov4.py

--- core/yolov4.py
+++ core/yolov4.py
@@ -125,7 +125,7 @@ def YOLOv4(input_layer, NUM_CLASS):
     conv = common.convolutional(conv, (1, 1, 1024, 512))
     conv = common.convolutional(conv, (3, 3, 512, 1024))
     conv = common.convolutional(conv, (1, 1, 1024, 512))
-    conv = common.convolutionalLSTM(conv, (3, 3, 512, 1024)) # Add one LSTM layer at the end
+   # conv = common.convolutionalLSTM(conv, (3, 3, 512, 1024)) # Add one LSTM layer at the end
     conv = common.convolutional(conv, (3, 3, 512, 1024))
     conv = common.convolutional(conv, (1, 1, 1024, 512))
 
